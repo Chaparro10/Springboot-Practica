55bf05599455787828e4377a08b98c62dcf64cbd
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-04-01 11:59:40 -0700

    [Feat]-Se agrega ResponseEntity para mejorar respuestas de controlado

--- platzi_market/src/main/java/com/market/platzi_market/web/controllers/ProductController.java
+++ platzi_market/src/main/java/com/market/platzi_market/web/controllers/ProductController.java
@@ -4,6 +4,8 @@ package com.market.platzi_market.web.controllers;
 import com.market.platzi_market.domain.service.ProductService;
 import com.market.platzi_market.persistence.entity.Product_mysql;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,30 +19,30 @@ public class ProductController {
 
 
     @GetMapping("")
-    private List<Product_mysql> getAll(){
-        return productService.getAll();
+    private ResponseEntity<List<Product_mysql>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     //por params
     @GetMapping("/{id_categoria}")
-    private List<Product_mysql> getByCatgoriaId(@PathVariable("id_categoria") int id_categoria){
-        return productService.getProductsByCategory(id_categoria);
+    private ResponseEntity <List<Product_mysql>> getByCatgoriaId(@PathVariable("id_categoria") int id_categoria){
+        return new ResponseEntity<>(productService.getProductsByCategory(id_categoria),HttpStatus.OK);
     }
 
     //por query
     /// categoria?id_categoria=1
     @GetMapping("/categoria")
-    private List<Product_mysql> getByCatgoria(@RequestParam int id_categoria, @RequestParam(required = false) String name){
-        return productService.getProductsByCategory(id_categoria);
+    private ResponseEntity<List<Product_mysql>> getByCatgoria(@RequestParam int id_categoria, @RequestParam(required = false) String name){
+        return new ResponseEntity<>(productService.getProductsByCategory(id_categoria),HttpStatus.OK);
     }
 
     @PostMapping("")
-    private Product_mysql save (@RequestBody Product_mysql product_mysql){
-            return productService.save(product_mysql);
+    private ResponseEntity<Product_mysql> save (@RequestBody Product_mysql product_mysql){
+            return  new ResponseEntity<>(productService.save(product_mysql),HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    private void delete(@PathVariable Long  id){
+    private void  delete(@PathVariable Long  id){
         productService.delete(id);
     }
 }

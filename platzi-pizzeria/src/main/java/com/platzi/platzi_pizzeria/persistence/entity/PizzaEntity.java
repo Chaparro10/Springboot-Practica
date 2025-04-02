a88d2bd5723505762ebbc20a89e23565fb9a1e5d
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-04-01 18:28:33 -0700

    [Feat]-Se agrega configuracion de BD y Hibernate

---
+++ platzi-pizzeria/src/main/java/com/platzi/platzi_pizzeria/persistence/entity/PizzaEntity.java
@@ -0,0 +1,32 @@
+package com.platzi.platzi_pizzeria.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pizza")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PizzaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(precision = 2)
+    private Double price;
+    @Column(columnDefinition = "TINYINT")
+    private Boolean vegetarian;
+    @Column(columnDefinition = "TINYINT")
+    private Boolean vegan;
+    @Column(columnDefinition = "TINYINT")
+    private Boolean available;
+}

d4c98d597512b295d15a86d50419603d278b6c6b
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-04-24 15:44:13 -0700

    Feat:----Crear autenticacion en memoria----

--- platzi-pizzeria/src/main/java/com/platzi/platzi_pizzeria/web/config/SecurityConfig.java
+++ platzi-pizzeria/src/main/java/com/platzi/platzi_pizzeria/web/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,13 +20,30 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http)throws Exception{
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         http.csrf(csrf->csrf.disable()).
                 cors(Customizer.withDefaults()).
                 authorizeHttpRequests(customizeRequest ->{
-            customizeRequest.anyRequest().authenticated().
-                    requestMatchers(HttpMethod.GET,"/api/**").permitAll();
+            customizeRequest
+                    .requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                    .anyRequest().authenticated();
                 });
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService memoryUser(){
+        UserDetails admin = User.builder()
+                .username("ADMIN")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN")
+                .build();
+            return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }

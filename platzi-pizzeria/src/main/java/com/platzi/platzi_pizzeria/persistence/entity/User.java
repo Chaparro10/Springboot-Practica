523f4c31def5f23022457ffb1568bcb14a87039b
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-04-24 16:09:11 -0700

    Feat:-----AUTENTICACION CON BD----

---
+++ platzi-pizzeria/src/main/java/com/platzi/platzi_pizzeria/persistence/entity/User.java
@@ -0,0 +1,23 @@
+package com.platzi.platzi_pizzeria.persistence.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "User")
+public class User {
+    @Id
+    @Column(nullable = false,length = 20)
+    private String username;
+    @Column(length = 50)
+    private String email;
+    @Column(nullable = false,columnDefinition = "TINYINT")
+    private Boolean locked;
+    @Column(nullable = false,columnDefinition = "TINYINT")
+    private Boolean disabled;
+}

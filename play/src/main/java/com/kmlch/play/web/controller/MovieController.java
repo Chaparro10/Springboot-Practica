db14923447897a2a96d317243b9ac7dceefa6013
kevinchaparro <19170614@itculiacan.edu.mx>
2026-01-08 09:59:28 -0700

    Validaciones automaticas con @Valid (validando datos de entrada con DTO)

--- play/src/main/java/com/kmlch/play/web/controller/MovieController.java
+++ play/src/main/java/com/kmlch/play/web/controller/MovieController.java
@@ -5,6 +5,7 @@ import com.kmlch.play.domain.dto.MovieDto;
 import com.kmlch.play.domain.dto.UpdateMovieDto;
 import com.kmlch.play.domain.services.MovieService;
 import com.kmlch.play.persistence.entity.MovieEntity;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,11 +41,11 @@ public class MovieController {
     }
 
     @PostMapping("")
-    public ResponseEntity<MovieDto> save(@RequestBody MovieDto movieDto){
+    public ResponseEntity<MovieDto> save( @Valid @RequestBody MovieDto movieDto){
             return ResponseEntity.status (HttpStatus.CREATED).body(this.movieService.save(movieDto));
     }
     @PatchMapping("/change")
-    public ResponseEntity<MovieDto> update(@RequestParam Long id, @RequestBody UpdateMovieDto updateMovieDto){
+    public ResponseEntity<MovieDto> update(@RequestParam Long id, @RequestBody @Valid UpdateMovieDto updateMovieDto){
         return ResponseEntity.ok(this.movieService.update(id,updateMovieDto));
     }
 }
